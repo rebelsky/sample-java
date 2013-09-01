8cba810ded3cf33f3b1cd84625a359b6e722592d
Samuel A.Rebelsky <rebelsky@grinnell.edu>
2013-09-01 15:03:21 -0500

    Add output to HelloWorld.

--- src/edu/grinnell/csc207/rebelsky/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/rebelsky/hello/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
 	System.out.println("Hello, Git");
+	System.out.println("Don't you love communicating this way?");
     }
 
 }
