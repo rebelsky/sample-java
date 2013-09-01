f1c31b88f034d0f211718a78e8bc4338665995fd
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2013-09-01 14:59:20 -0500

    Add Java code.

---
+++ src/edu/grinnell/csc207/rebelsky/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package edu.grinnell.csc207.rebelsky.hello;
+
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println("Hello, Git");
+    }
+
+}
